490ef7207752eb1b50aa0f12c8a9a8c49a571786
soeroesg <gabor.soros@gmail.com>
2021-11-30 23:08:41 +0100

    upgraded ssd-access and scd-access libraries so that we can submit the auth0 context to them

--- src/pages/scd/index.svelte
+++ src/pages/scd/index.svelte
@@ -6,8 +6,12 @@
 <script>
     import { authStore } from '@oarc/scd-access/authstore.js'
 
+    let auth_domain = oscp_app.env["AUTH0_SCD_DOMAIN"]
+    let auth_client_id = oscp_app.env["AUTH0_SCD_CLIENTID"]
+    let auth_audience = oscp_app.env["AUTH0_SCD_AUDIENCE"]
+    let auth_scope = oscp_app.env["AUTH0_SCD_SCOPE"]
     // need to queue init till after Routify has been initialized
-    setTimeout(() => window.routify.inBrowser && authStore.init())
+    setTimeout(() => window.routify.inBrowser && authStore.init(auth_domain, auth_client_id, auth_audience, auth_scope))
 </script>
 
 
--- src/pages/ssd/index.svelte
+++ src/pages/ssd/index.svelte
@@ -6,8 +6,12 @@
 <script>
     import { authStore } from '@oarc/ssd-access/authstore.js'
 
+    let auth_domain = oscp_app.env["AUTH0_SSD_DOMAIN"]
+    let auth_client_id = oscp_app.env["AUTH0_SSD_CLIENTID"]
+    let auth_audience = oscp_app.env["AUTH0_SSD_AUDIENCE"]
+    let auth_scope = oscp_app.env["AUTH0_SSD_SCOPE"]
     // need to queue init till after Routify has been initialized
-    setTimeout(() => window.routify.inBrowser && authStore.init())
+    setTimeout(() => window.routify.inBrowser && authStore.init(auth_domain, auth_client_id, auth_audience, auth_scope))
 </script>
 
 
