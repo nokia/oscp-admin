1fb59d405343e5f319aa471a37dd4eb72a9200f5
Tamas Rigo <tamas.rigo@nokia.com>
2024-01-09 13:03:54 +0100

    add typescript plugin

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import { routify } from '@sveltech/routify';
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), basicSsl(), routify({ dynamicImports: true })],
+    plugins: [svelte(), basicSsl(), routify({ dynamicImports: true }), typescript()],
     optimizeDeps: {
-      exclude: ["@sveltech/routify"],
+        exclude: ['@sveltech/routify'],
     },
 });
